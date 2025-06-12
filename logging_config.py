b1ef1326c297091eabebbbad75e36e02641340a8
surachad1next <surachad.t@nextroboticslab.com>
2025-06-12 09:21:57 +0700

    update logger

--- logging_config.py
+++ logging_config.py
@@ -1,9 +1,14 @@
 import logging
 import sys
+from logging.handlers import RotatingFileHandler
 
 def setup_logger(name="app", logfile="sys.log"):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+    
+    file_handler = RotatingFileHandler(logfile, maxBytes=5 * 1024 * 1024, backupCount=3)
+    file_handler.setLevel(logging.INFO)
+    
     formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
     stream_handler = logging.StreamHandler(sys.stdout)
