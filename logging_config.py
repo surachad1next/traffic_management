95d36a0e603d0150a366e9199b52ca0da83b3de7
surachad1next <surachad.t@nextroboticslab.com>
2025-08-22 10:53:49 +0700

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -1,23 +1,30 @@
 import logging
 import sys
 from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 
 def setup_logger(name="app", logfile="sys.log"):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    
-    file_handler = RotatingFileHandler(logfile, maxBytes=5 * 1024 * 1024, backupCount=3)
-    file_handler.setLevel(logging.INFO)
-    
-    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(formatter)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
-    file_handler = logging.FileHandler(logfile)
-    file_handler.setFormatter(formatter)
+        # ✅ หมุนไฟล์ทุกวัน เวลาเที่ยงคืน เก็บย้อนหลัง 7 วัน
+        file_handler = TimedRotatingFileHandler(
+            logfile, when="midnight", interval=1, backupCount=7, encoding="utf-8"
+        )
+        # file_handler = RotatingFileHandler(
+        #     logfile, maxBytes=5 * 1024 * 1024, backupCount=3, encoding='utf-8'
+        # )
+        file_handler.setFormatter(formatter)
 
-    logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+
+        logger.propagate = False
 
     return logger
